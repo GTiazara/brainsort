9523a95314eb5d92d6bc36226a550c4d29623f66
gtiazara <tiazaragaetan@gmail.com>
2025-11-13 20:44:16 +0100

    first version deployable on versel

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "brainsort-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./click.wav",
+  "./success.wav",
+  "./fail.wav"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
